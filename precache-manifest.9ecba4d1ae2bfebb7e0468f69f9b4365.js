eb6b2b89670b5f0fd486f025f512d9c1466a5ede
Alka Priya <priyaa@vmware.com>
2020-07-08 22:17:55 +0530

    Updates

---
+++ precache-manifest.9ecba4d1ae2bfebb7e0468f69f9b4365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10734ec7a2961f2355fff0dd93c1daa5",
+    "url": "/Priyesh-Git/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c5b14692b4d6da45efbd",
+    "url": "/Priyesh-Git/monsters-rolodex/static/css/main.d8d3b0f6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Priyesh-Git/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Priyesh-Git/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b14692b4d6da45efbd",
+    "url": "/Priyesh-Git/monsters-rolodex/static/js/main.068e17a0.chunk.js"
+  },
+  {
+    "revision": "d974fd95386b9bff7220",
+    "url": "/Priyesh-Git/monsters-rolodex/static/js/runtime-main.c412c4c5.js"
+  }
+]);
